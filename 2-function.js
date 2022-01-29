f3c3b8897c224f5d6e51da85239322554d4c9b47
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-01-30 00:17:36 +0600

    module-19 added

---
+++ 2-function.js
@@ -0,0 +1,19 @@
+// 19-2 Declare a Function, call function, function vs loop
+
+// function declaration
+function startFan() {
+    console.log('walk towards the switch')
+    console.log('press the switch');
+}
+
+// call the function
+startFan();
+
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+// startFan();
+var country = "bd";
+var city = 'Dhaka';
+var location = 'Gulisthan';
+startFan();
