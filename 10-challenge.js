0bb9ff52d99c1f6ca9198b602c3ce3daad95a541
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-02-06 21:54:05 +0600

    challeng part added

---
+++ 10-challenge.js
@@ -0,0 +1,16 @@
+
+নিচের ভেরিয়েবল ডিক্লেয়ার এ কোনটার মধ্যে কি কি সমস্যা আছে। দেখতো বের করতে পারো কিনা ?
+
+    Var price = 33  // Var is wrong = var is right
+
+var name - Shabana  // string can't be subtract
+
+var boxName = ‘Cocola;  // cotetion related error
+
+var 88_price = 34;  // variable name can't start with numerical number
+
+var enum = -1;  // variable name is not meaningful
+
+var _$box’78 = ‘Monika’;  // variable name can't contain "’" kind of symbol.
+
+var home-address = “kochu khet”;  // variable name can not contain "-" is middle part of name
