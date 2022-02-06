0e042002d2b68457b5fc9ccd673037b599d1f2ab
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-02-06 23:03:38 +0600

    module-18 updated

--- 4-push-pop.js
+++ 4-push-pop.js
@@ -9,13 +9,22 @@ lastBench.push('nalam');
 
 var friendsAge = [11, 13, 17, 12];
 // friendsAge.push(15);
-console.log(friendsAge);
+// console.log(friendsAge);
 
 // remove an element from an array
 var lastItem = friendsAge.pop();
 // friendsAge.pop();
-console.log(friendsAge);
-console.log(lastItem);
+// console.log(friendsAge);
+// console.log(lastItem);
+
 
 // javascript remove first item from an array !!
-// javascript array add element in the beginning !!!
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+fruits1.shift();
+// console.log(fruits1)
+
+
+// javascript array add element in the beginning !!
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+fruits2.unshift("Lemon", "Pineapple");
+// console.log(fruits2)
