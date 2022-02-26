0b63c9e0ba2d096195219ec326c4ec4b2499b9c2
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-02-26 23:35:59 +0600

    33-3 JSON placeholder, GET data, display data on UI

--- js/json.js
+++ js/json.js
@@ -1,24 +1,23 @@
-// json = javascript object notation
-
-const user = { id: '11', name: 'amir', job: 'actor' };
+// JavaScript Object Notation
+// JSON
+const user = { id: 11, name: 'Gorib Amir', job: 'actor' };
 const stringified = JSON.stringify(user);
 // console.log(user);
 // console.log(stringified);
 
 const shop = {
-    name: 'alia',
-    shop: 'RanbirShop',
-    products: ['shoes', 'shirts', 'pants'],
+    name: 'Alia Store',
+    address: 'Ranbir road',
+    profit: 15000,
+    products: ['laptop', 'mobile', 'pepsi'],
     owner: {
-        name: 'alia butt',
+        name: 'Alia Bhatt',
         profession: 'actor'
     },
     isExpensive: false
-}
+};
 const shopStringified = JSON.stringify(shop);
 // console.log(shop);
 console.log(shopStringified);
-console.log(shopStringified.owner); // Stringified String = not possible because stringify can't use object property
 const converted = JSON.parse(shopStringified);
-// console.log(converted);
 console.log(converted.owner);
