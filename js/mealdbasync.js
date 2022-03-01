d0cd8cbc613944ab903b20b9c125bd4117cdfb47
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-03-01 22:21:12 +0600

    34-8 (advanced) Remove previous result and async await

--- js/mealdbasync.js
+++ js/mealdbasync.js
@@ -1,4 +1,4 @@
-const searchFood = () => {
+const searchFood = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
@@ -10,10 +10,14 @@ const searchFood = () => {
     else {
         // load data
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displaySearchResult(data.meals))
-            .catch(error => displayError(error));
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySearchResult(data.meals)
+
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => displaySearchResult(data.meals))
+        //     .catch(error => displayError(error));
     }
 }
 
@@ -40,16 +44,22 @@ const displaySearchResult = meals => {
     })
 }
 
-const loadMealDetail = mealId => {
+const loadMealDetail = async mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMealDetail(data.meals[0]));
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetail(data.meals[0]);
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => displayMealDetail(data.meals[0]));
 }
+
 
 const displayMealDetail = meal => {
     console.log(meal);
     const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
