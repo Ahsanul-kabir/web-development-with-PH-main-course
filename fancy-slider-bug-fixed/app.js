a5b78b330971b1cb3e7da81372e5eba6319c6ad5
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-03-12 21:00:59 +0600

    43_5-3 Use Console log to debug and fix the slider

--- fancy-slider-bug-fixed/app.js
+++ fancy-slider-bug-fixed/app.js
@@ -4,7 +4,7 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-// selected image 
+// selected image
 let sliders = [];
 
 
@@ -13,7 +13,7 @@ let sliders = [];
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
-// show images 
+// show images
 const showImages = (images) => {
   imagesArea.style.display = 'block';
   gallery.innerHTML = '';
@@ -31,7 +31,7 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
@@ -39,7 +39,7 @@ let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
   element.classList.add('added');
- 
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
@@ -58,7 +58,7 @@ const createSlider = () => {
   sliderContainer.innerHTML = '';
   const prevNext = document.createElement('div');
   prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
-  prevNext.innerHTML = ` 
+  prevNext.innerHTML = `
   <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
   <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
   `;
@@ -83,7 +83,7 @@ const createSlider = () => {
   }, duration);
 }
 
-// change slider index 
+// change slider index
 const changeItem = index => {
   changeSlide(slideIndex += index);
 }
