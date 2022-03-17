4f550e9e711b3b53afebbf37caa9cf27368991e7
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-03-18 00:40:07 +0600

    46-4 Components inside the folder structure and connect them

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Countries from './components/Countries/Countries';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Countries />
-      <Header></Header>
     </div>
   );
 }
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Countries.css';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data));
+
+    }, [])
+
     return (
         <div>
-            <h1>This is from the Countries Component</h1>
+            <h2>Hello Mello Yello Gello from Countries: {countries.length}</h2>
+            <div>
+                {
+                    countries.map(country => console.log(country))
+                }
+            </div>
         </div>
     );
 };
