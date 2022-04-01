1e32e871c1c56fe03e17e5aa2364ed3a50138c8c
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-02 00:51:41 +0600

    52-4 Set Header, Nav, Create Link, display active route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to my fancy routing website</h2>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
