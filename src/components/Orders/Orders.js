819a8f12c95b57fe6e61614aa8b0910449565905
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-03 12:04:45 +0600

    53-3 Write useState and useEffect in a different file and import it

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
     return (
         <div>
             <h2>This is Orders</h2>
