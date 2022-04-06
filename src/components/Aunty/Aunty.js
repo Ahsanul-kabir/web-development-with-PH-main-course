0fe3433c4651aae127cd4536179a309b88176d00
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-06 19:53:58 +0600

    54-7 Simple Introduction to Context API to avoid Prop drilling

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Aunty = ({ house }) => {
+    const ring = useContext(RingContext);
     return (
         <div>
             <h4>Aunty</h4>
             <p>House: {house}</p>
+            <p><small>Ring: {ring}</small></p>
         </div>
     );
 };
 
-export default Aunty; 
+export default Aunty;
