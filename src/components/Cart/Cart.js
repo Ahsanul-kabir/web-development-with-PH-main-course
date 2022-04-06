7d36d7d90c10cd040e6f459f39c0a9769a45e731
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-06 10:39:13 +0600

    54-3 Recap Remove from cart and not allow duplicate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = () => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
     return (
         <div>
-            <h1>This is Cart</h1>
+            <h2>Items Selected: {cart.length}</h2>
+
+            {
+                cart.map(tShirt => <p>
+                    {tShirt.name}
+                    <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
+                </p>)
+            }
         </div>
     );
 };
