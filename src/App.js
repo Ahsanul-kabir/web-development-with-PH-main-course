8a8a78280ce4aa45de93a0e59764378bab523f0c
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-07 11:35:20 +0600

    56-1 Module Introduction, authentication vs Authorization

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Authentication :  Who am I</p>
+      <p>Authorization :  Who am I + make some power(likes, Admin, General person)</p>
     </div>
   );
 }
