9785075fd7e36a6594ffb53612bb220daab72c87
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-08 00:16:58 +0600

    56-8 Module Summary and display user info

--- src/App.js
+++ src/App.js
@@ -35,22 +35,10 @@ function App() {
   const handleGithubSignIn = () => {
     signInWithPopup(auth, githubProvider)
       .then((result) => {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const credential = GithubAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-
-        // The signed-in user info.
         const user = result.user;
-        // ...
+        setUser(user);
       }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GithubAuthProvider.credentialFromError(error);
-        // ...
+        console.log('error', error)
       });
   }
 
@@ -58,7 +46,7 @@ function App() {
     <div className="App">
       {/* { condition ? true : flase} */}
       {
-        user.email ? <button onClick={handleSignOut}>Sign out</button>
+        user.id ? <button onClick={handleSignOut}>Sign out</button>
           :
           <>
             <button onClick={handleGoogleSignIn}>Google Sign In</button>
