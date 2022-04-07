79d72ac6c80fd0c4a2c1c9652b930e4e61967659
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-08 00:11:56 +0600

    56-7 Implement github login system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, signInWithPopup, signOut } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { useState } from 'react';
 
 // Initialize Firebase Authentication and get a reference to the service
@@ -9,10 +8,11 @@ const auth = getAuth(app);
 
 function App() {
   const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
         setUser(user);
@@ -31,13 +31,39 @@ function App() {
       });
   }
 
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
   return (
     <div className="App">
       {/* { condition ? true : flase} */}
       {
         user.email ? <button onClick={handleSignOut}>Sign out</button>
           :
-          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+            <button onClick={handleGithubSignIn}>Github Sign In</button>
+          </>
       }
       <h2>Name: {user.displayName}</h2>
       <p>I know your email address: {user.email}</p>
