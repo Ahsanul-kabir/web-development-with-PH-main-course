408c6d4a805956a42a4ebc479c950b856f04f19a
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-08 14:53:48 +0600

    57-1 Login, Registration, Sign up, Sign in, project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from './firebase.init';
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" />
+        <input type="password" name="" id="" />
+      </form>
     </div>
   );
 }
