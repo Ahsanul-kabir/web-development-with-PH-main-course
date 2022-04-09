011efbb3665d593a013887c695a93e45023156e6
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-09 17:33:39 +0600

    58-4 Implement Sign out and display user logged in User name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
-    const { user } = useFirebase({});
+    const { user, handleSignOut } = useFirebase({});
     return (
         <div className='header'>
             <nav>
@@ -12,7 +12,8 @@ const Header = () => {
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
                 <Link to='/register'>Register</Link>
-                {user.uid ? <button>SignOut</button> : <Link to='/login'>Login</Link>}
+                <span>{user?.displayName && user.displayName} </span>
+                {user?.uid ? <button onClick={handleSignOut}>SignOut</button> : <Link to='/login'>Login</Link>}
             </nav>
         </div>
     );
