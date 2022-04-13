221681673c0a1d6a97561cecdd053d626000a85f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-14 00:03:57 +0600

    60-3 Collect, Edit, optimize and host images for your website

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Services from './Pages/Home/Services/Services';
 import About from './Pages/About/About';
 import Header from './Shared/Header/Header';
 import Footer from './Shared/Footer/Footer';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={ } />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Services = () => {
     return (
         <div>
-
+            <h2>This is Services</h2>
         </div>
     );
 };
