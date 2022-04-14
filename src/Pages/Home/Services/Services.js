3ea9c643f8f1f4c31c6dd4d34904b8bfbd81834f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-14 16:19:50 +0600

    60-4 Load fake data map services to display single services

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            <h2>This is Services</h2>
+            <h2>This is Services {services.length}</h2>
+            {
+                services.map(service => <Service key={service.id} service={service} />)
+            }
         </div>
     );
 };
