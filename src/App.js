86cac08f7ed0063d413cf84abb024eb4327cf3fa
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-16 23:48:46 +0600

    folder stucture wrong place fixed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Footer from './Pages/Shared/Footer/Footer';
 import Home from './Pages/Home/Home/Home';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import NotFound from './Pages/Shared/NotFound/NotFound';
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
