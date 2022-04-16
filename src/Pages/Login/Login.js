d52067de23ca2b7bbc730a969d7819e4f172e7eb
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-16 23:37:11 +0600

    61-7 Email password based authentication with react firebase hooks

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const navigate = useNavigate()
@@ -9,13 +11,26 @@ const Login = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(emailRef.current.value);
-        console.log(passwordRef.current.value);
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password);
     }
 
     const navigateRegister = () => {
         navigate('/register')
     }
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
