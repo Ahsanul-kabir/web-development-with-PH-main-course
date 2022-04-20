6a54720c12c8a7c2092dff7815d6759dde26d842
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-20 20:08:38 +0600

    64-4 Create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
     res.send('Hello World!!');
 })
 
+const users = [
+    { id: 1, name: 'Sabana', email: 'Sabana@gmail.com', phone: '017888899' },
+    { id: 2, name: 'Sabanor', email: 'Sabanor@gmail.com', phone: '017877899' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '017228899' },
+    { id: 4, name: 'Sabila', email: 'Sabila@gmail.com', phone: '0178888555' },
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params)
+    const id = parseInt(req.params.id)
+    const user = users.find(user => user.id == id)
+    res.send(user)
+})
+
 app.listen(port, () => {
     console.log('Listening on port', port)
 })
