8c066fb34020c26ed0f443b83eff847c94e2162f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-21 19:59:19 +0600

    65-3 Send data to database using mongodb insertOne

--- index.js
+++ index.js
@@ -10,16 +10,23 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://freshDale:freshDale123456@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://freshDale:freshDale12345@cluster0.vl9uy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('DB connect');
 
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        const user = { name: 'Keka Queen', email: 'keka@gmail.com' }
+        const result = await userCollection.insertOne(user)
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
