ff7afdfde17e7db5cb7752d79a527ca63ccad7ab
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-21 22:52:49 +0600

    65-4 (Recap) Async await, try catch and mongodb connection

---
+++ src/components/AddUsers/AddUsers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUsers = () => {
+    return (
+        <div>
+            <h1>Please Add a New User</h1>
+        </div>
+    );
+};
+
+export default AddUsers;
