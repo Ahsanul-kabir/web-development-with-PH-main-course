dec4b9e134d26e1c72e06df2eba5a544755a2f78
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-23 00:39:36 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import AddUsers from './components/AddUsers/AddUsers';
 import Home from './components/Home/Home';
 import UpdateUser from './components/UpdateUser/UpdateUser';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="user/add" element={<AddUsers />} />
