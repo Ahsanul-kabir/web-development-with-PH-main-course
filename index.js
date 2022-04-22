a5037a1b73df6078974cb06b637016c1b4066b21
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-22 17:26:59 +0600

    65-7 Load and display data from database, create delete button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require(mongodb).ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,6 +20,7 @@ async function run() {
         await client.connect();
         const userCollection = client.db("foodExpress").collection("user");
 
+        // get users
         app.get('/user', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
@@ -32,6 +34,13 @@ async function run() {
             console.log('Adding uew user', newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result);
+        })
+
+        // delete user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+
         })
     }
     finally {
