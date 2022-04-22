2646b34e296b30c454bf51ad7c87dcca1be24920
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-22 20:04:44 +0600

    65-8 Delete a user from the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require(mongodb).ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +28,14 @@ async function run() {
             res.send(users);
         })
 
+        // update user
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.findOne(query)
+            res.send(result);
+        })
+
         // POST User : Add a new User
         app.post('/user', async (req, res) => {
             const newUser = req.body;
@@ -40,7 +48,8 @@ async function run() {
         app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
-
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
