4ef12cb48ed9ea23ca9affe9e00960336064709f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-22 20:03:18 +0600

    65-8 Delete a user from the database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AddUsers from './components/AddUsers/AddUsers';
 import Home from './components/Home/Home';
+import UpdateUser from './components/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="user/add" element={<AddUsers />} />
+        <Route path="/update/:id" element={<UpdateUser />} />
       </Routes>
     </div>
   );
