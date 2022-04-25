6c02464feabbd511c9fb75d537a3288f3a30fad9
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-25 16:41:03 +0600

    66-7 Manage Services and explore delete api

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
             const result = await serviceCollection.insertOne(newServices);
             res.send(result);
         })
+
+        // delete service
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
