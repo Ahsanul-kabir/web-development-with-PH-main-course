bc320d57f0e69cd3b5b45fbfbfcd2810f0cb4065
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-05 01:29:07 +0600

    68-5 Filter orders info by email address Introduction JWT token

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
         })
 
         // Order Collection API
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
