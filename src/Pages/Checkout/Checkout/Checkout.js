66d141c3a37c4fa3b80de13927ac5c2bdbbb83a2
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-04 16:06:23 +0600

    68-2 (Optional) Handle Controlled input value change in your form

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../hooks/useServiceDetails';
 
@@ -7,14 +7,38 @@ const Checkout = () => {
 
     const [service] = useServiceDetails(serviceId);
 
-    console.log(service);
+    const [user, setUser] = useState({
+        name: 'Akbar The Great',
+        email: 'akbar@momo.taj',
+        address: 'Tajmohol Road Md.pur',
+        phone: '01711111111'
+    });
+
+    const handleAddressChange = event => {
+        console.log(event.target.value);
+        const { address, ...rest } = user;
+        const newAddress = event.target.value;
+        const newUser = { address: newAddress, ...rest };
+        console.log(newUser);
+        setUser(newUser);
+    }
 
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
 
-            <form className='w-50 mx-auto'>
-                <input type="text" name="name" placeholder='name' required />
+            <form >
+                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='name' required />
+                <br />
+                <input className='w-100 mb-2' type="email" value={user.email} name="email" placeholder='email' required />
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='service' required />
+                <br />
+                <input className='w-100 mb-2' type="text" onChange={handleAddressChange} value={user.address} name="address" placeholder='address' required />
+                <br />
+                <input className='w-100 mb-2' type="text" name="phone" placeholder='phone' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
         </div>
     );
