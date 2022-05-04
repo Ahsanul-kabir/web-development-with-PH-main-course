d0435f80c5d2faec247887f974dbbce79bf79f9b
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-04 20:05:49 +0600

    68-3 Get Order data, create API endpoint and readonly input

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,40 +1,55 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../hooks/useServiceDetails';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Checkout = () => {
     const { serviceId } = useParams();
 
     const [service] = useServiceDetails(serviceId);
 
-    const [user, setUser] = useState({
-        name: 'Akbar The Great',
-        email: 'akbar@momo.taj',
-        address: 'Tajmohol Road Md.pur',
-        phone: '01711111111'
-    });
+    const [user] = useAuthState(auth);
 
-    const handleAddressChange = event => {
-        console.log(event.target.value);
-        const { address, ...rest } = user;
-        const newAddress = event.target.value;
-        const newUser = { address: newAddress, ...rest };
-        console.log(newUser);
-        setUser(newUser);
+    // const [user, setUser] = useState({
+    //     name: 'Akbar The Great',
+    //     email: 'akbar@momo.taj',
+    //     address: 'Tajmohol Road Md.pur',
+    //     phone: '01711111111'
+    // });
+
+    // const handleAddressChange = event => {
+    //     console.log(event.target.value);
+    //     const { address, ...rest } = user;
+    //     const newAddress = event.target.value;
+    //     const newUser = { address: newAddress, ...rest };
+    //     console.log(newUser);
+    //     setUser(newUser);
+    // }
+
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: event.target.email.value,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
     }
 
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
 
-            <form >
-                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='name' required />
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='name' required readOnly disabled />
                 <br />
-                <input className='w-100 mb-2' type="email" value={user.email} name="email" placeholder='email' required />
+                <input className='w-100 mb-2' type="email" value={user?.email} name="email" placeholder='email' required />
                 <br />
                 <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='service' required />
                 <br />
-                <input className='w-100 mb-2' type="text" onChange={handleAddressChange} value={user.address} name="address" placeholder='address' required />
+                <input className='w-100 mb-2' type="text" name="address" placeholder='address' autoComplete='off' required />
                 <br />
                 <input className='w-100 mb-2' type="text" name="phone" placeholder='phone' required />
                 <br />
