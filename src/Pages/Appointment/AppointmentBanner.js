e7965829ea936215049cc0c25215b9ce005c8f6c
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-16 12:50:33 +0600

    72-9 Module summary

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
+import bgImg from '../../assets/images/bg.png'
 
 // React Day Picker used
 import { DayPicker } from 'react-day-picker';
@@ -9,7 +10,7 @@ import { format } from 'date-fns';
 const AppointmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen">
+        <div class="hero min-h-screen" style={{ backgroundImage: `url(${bgImg})` }}>
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} alt='' class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
