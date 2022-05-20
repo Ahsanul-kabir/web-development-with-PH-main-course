0083c6bb0a787667033ef04f7be6820476e0ed1f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-20 15:47:57 +0600

    74-2 API naming convention and simple Booking API

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -26,7 +27,21 @@ async function run() {
             res.send(services);
         })
 
+        /**
+        * API Naming Convention
+        * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+        * app.get('/booking/:id') // get a specific booking
+        * app.post('/booking') // add a new booking
+        * app.patch('/booking/:id) //
+        * app.delete('/booking/:id) //
+       */
 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send(result);
+        })
     }
     finally {
 
